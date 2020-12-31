ae03b34e2233332e83b56233b657c054265e928c
Ben Cho <ben.cho@gmail.com>
2020-12-30 21:08:29 -0700

    update to version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="quilter",
-    version="0.0.2",
+    version="0.0.3",
     author="Ben Cho",
     license="MIT License",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     author_email="ben.cho@gmail.com",
